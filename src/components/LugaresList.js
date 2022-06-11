69da60c6b4409949f1ad85251826f990ff5b1a89
Estebnlr <99986563+Estebnlr@users.noreply.github.com>
2022-06-10 23:38:48 -0500

    xd

--- src/components/LugaresList.js
+++ src/components/LugaresList.js
@@ -7,22 +7,20 @@ export default function ProductosList({ misCategorias })
 
   return (
     <div className="row mt-4">
-      {arrProductos.map(({ prod_nom, prod_desc, prod_img, categoriaId, prod_id }, i) => (
+      {arrProductos.map(({ prod_nom, prod_prec, prod_img, categoriaId, prod_id }, i) => (
         <div className="col-12 col-md-6 col-lg-3" key={i}>
-          <div className="card mb-4">
-            <img
-              src={prod_img}
-              className="card-img-top img-fluid"
-              alt={prod_nom}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{prod_nom}</h5>
-              <p className="card-text">{prod_desc}</p>
-              <Link className="btn btn-danger" to={`/detalleproducto/${categoriaId}/${prod_id}`}>
-                Conocer Producto
-              </Link>
-            </div>
-          </div>
+
+          <div className="card-xd mb-3 ">
+              <Link className="btn" to={`/categoria/${categoriaId}`}>
+                <img src={prod_img} className="card-img-top " alt={prod_nom} />
+                <div className="card-body">
+                  <h6 className="card-text text-dark">{prod_nom}</h6>
+                  <h5 className="card-text text-danger">
+                    $/ {prod_prec}
+                  </h5>
+                </div> </Link>
+              </div>
+          
         </div>
       ))}
     </div>
